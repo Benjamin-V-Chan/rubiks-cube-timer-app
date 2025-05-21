c2d094963f362393e7637d485d295fedc76b24da
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-20 20:56:19 -0700

    statistical_functions setup and list

---
+++ statistical_functions.py
@@ -0,0 +1,17 @@
+def sort(data):
+    return
+
+def average(data):
+    return
+
+def min(data):
+    return
+
+def max(data):
+    return
+
+def median(data):
+    return
+
+def quartile(data):
+    return
