c6b9f4969c057fa24e0187f5c878e67919bc14eb
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-20 21:54:32 -0700

    main outline

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 import time
+import pygame
+import sys
 
+
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+# SETUP
+pygame.init()
+display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pygame Template")
+clock = pygame.time.Clock()
+
+
+# HELPER FUNCTIONS
+
+def main():
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Game state updates
+        
+        # Display updates
+        display.fill(BLACK)
+        
+        
+        
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
